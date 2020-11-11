56e858d255047080e7c46d1a421ba182d57a6683
Mustafa Tanis <mustafatanis97@gmail.com>
2020-11-11 14:33:19 -0500

    Practice

---
+++ src/Practice_11_11_2020/Array_LoopsPractice.java
@@ -0,0 +1,22 @@
+package Practice_11_11_2020;
+
+import java.util.Arrays;
+
+public class Array_LoopsPractice {
+    public static void main(String[] args) {
+
+        int [] scores = new int [5];
+
+        scores[0] = 10;
+        scores[1] = 20;
+        scores[2] = 50;
+        scores[3] = 30;
+        scores[4] = 40;
+
+        Arrays.sort(scores);
+        System.out.println(scores[1]); // 20
+        System.out.println(Arrays.toString(scores)); // [10, 20, 30, 40, 50] "softed"
+
+
+    }
+}
